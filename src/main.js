79071a47e93e63fb4f8c8731274687af1a5935e6
BigBlueHat <byoung@bigbluehat.com>
2020-03-25 21:30:51 -0400

    Apply new code styles to main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-var PouchDB = require('pouchdb');
-var Vue = require('vue');
+const PouchDB = require('pouchdb');
+const Vue = require('vue');
+
 Vue.config.debug = true;
 
-var db = new PouchDB('holst'); // !!! only temporary...changes to actual db
+let db = new PouchDB('holst'); // !!! only temporary...changes to actual db
 window.db = db;
 
 window.app = new Vue({
@@ -22,37 +23,33 @@ window.app = new Vue({
     confirmDelete: false,
     showSyncForm: false
   },
-  created: function() {
-    var self = this;
-    self.listDocs();
+  created() {
+    this.listDocs();
   },
   methods: {
-    setDBUrl: function() {
-      var self = this;
-      console.log(self.url);
+    setDBUrl() {
       // TODO: validate / force remote URL?
-      db = new PouchDB(self.url);
-      self.listDocs();
+      db = new PouchDB(this.url);
+      this.listDocs();
     },
-    listDocs: function() {
-      var self = this;
+    listDocs() {
+      const self = this;
       db.allDocs()
-        .then(function(resp) {
+        .then((resp) => {
           self.ids = {};
-          for (var i = 0; i < resp.rows.length; i++) {
-            self.ids[resp.rows[i].key] = {revs: [resp.rows[i].value.rev]};
+          for (let i = 0; i < resp.rows.length; i++) {
+            self.ids[resp.rows[i].key] = { revs: [resp.rows[i].value.rev] };
           }
-        }
-      );
+        });
     },
-    newDoc: function(e) {
-      var self = this;
+    newDoc() {
+      const self = this;
       if (self.new_doc_name !== '') {
         self.doc._id = self.new_doc_name;
         // save the doc immediately
         db.put(self.doc)
-          .then(function(resp) {
-            console.log('resp', resp);
+          .then((resp) => {
+            const doc = {};
             // store the rev, so we can PUT the update later
             doc._rev = resp.rev;
             // make the new doc the current doc
@@ -61,28 +58,27 @@ window.app = new Vue({
             self.new_doc_name = '';
             // reload the list of docs
             self.listDocs();
-          }).catch(console.log.bind(console));
+          }).catch(console.error.bind(console));
       } else {
         // TODO: um..yeah..error handling...
         alert('Documents need names...sorry.');
       }
     },
-    loadDoc: function(e, id) {
+    loadDoc(e, id) {
       e.preventDefault();
-      var self = this;
+      const self = this;
       db.get(id)
-        .then(function(doc) {
+        .then((doc) => {
           self.doc = doc;
-        }
-      );
+        });
     },
-    saveDoc: function(e) {
+    saveDoc(e) {
       e.preventDefault();
-      this.doc.updated = (new Date).toISOString();
+      this.doc.updated = (new Date()).toISOString();
       db.put(this.doc);
       // TODO: handle errors and stuff
     },
-    deleteDoc: function(e) {
+    deleteDoc(e) {
       e.preventDefault();
       if (this.confirmDelete) {
         db.remove(this.doc);
@@ -97,18 +93,18 @@ window.app = new Vue({
         this.confirmDelete = true;
       }
     },
-    syncTo: function() {
-      var self = this;
+    syncTo() {
+      const self = this;
       // TODO: maybe do some validation or something
-      var remote = new PouchDB(self.remote.url, {
+      const remote = new PouchDB(self.remote.url, {
         auth: {
           user: self.remote.user,
           password: self.remote.password
         }
       });
       PouchDB.sync(db, remote)
-        .on('complete', function(info) {
-          console.log('sync info', info);
+        .on('complete', (info) => {
+          console.info('sync info', info);
           alert('woot!');
           self.showSyncForm = false;
           self.listDocs();
